9f38d9c571fcde77ec3dbeb90c0c484e5dbe9488
Dima <shedi26@mail.ru>
2016-04-22 01:27:42 +0300

    HW3 music_library template

--- homeworks/music_library/bin/music_library.pl
+++ homeworks/music_library/bin/music_library.pl
@@ -3,5 +3,35 @@
 use strict;
 use warnings;
 
+use FindBin '$Bin';
+use lib "$Bin/../lib";
+
+use Local::MusicLibrary qw(parseEntry printTable);
+use Getopt::Long;
+
+#####
+use feature 'say';
+use DDP;
+#####
+
+my %opts;
+GetOptions (
+	'band=s'	=> \$opts{band},
+	'year=s'	=> \$opts{year},
+	'album=s'	=> \$opts{album},
+	'track=s'	=> \$opts{track},
+	'format=s'	=> \$opts{format},
+	'sort=s'	=> \$opts{sort},
+	'columns=s@'	=> \$opts{columns} );
+
+if ($opts{columns}) {@{$opts{columns}} = split (/,/, join (',', @{$opts{columns}}))}
+
+p %opts;
+my @libr;
+
+
 while (<>) {
+	push @libr, parseEntry($_);
 }
+
+printTable (\@libr, \%opts);
--- homeworks/music_library/lib/Local/MusicLibrary.pm
+++ homeworks/music_library/lib/Local/MusicLibrary.pm
@@ -21,4 +21,46 @@ our $VERSION = '1.00';
 
 =cut
 
+use Exporter 'import';
+our @EXPORT_OK = qw(parseEntry printTable);
+
+sub parseEntry {
+	my ($str) = @_;
+	$str =~ m{^[.]/(?<band>\w+)/(?<year>\d+)\s[-]\s(?<album>\w+)/(?<track>\w+)[.](?<format>\w+)$};
+	my %entry = (
+		band	=> $+{band},
+		year	=> $+{year},
+		album 	=> $+{album},
+		track	=> $+{track},
+		format	=> $+{format} );
+	return \%entry;
+}
+
+########
+use DDP;
+#######
+
+sub printTable {
+	my ($libr, $opts) = @_;
+	my @resultTable;
+#make with for
+	my %colWidth = (
+                band   => 0,
+                year    => 0,
+                album   => 0,
+                track   => 0,
+                format  => 0 );
+
+	for (@$libr) {
+		#p $$_{band};
+#make with for
+		if ($$opts{band} and $$opts{band} ne $$_{band}) {next}
+		if ($$opts{album} and $$opts{album} ne $$_{album}) {next}
+		if ($$opts{track} and $$opts{track} ne $$_{track}) {next}
+		if ($$opts{format} and $$opts{format} ne $$_{format}) {next}
+		if ($$opts{band} and $$opts{band} ne $$_{band}) {next}
+		push @resultTable, $_;
+	}
+	p @resultTable;
+}
 1;
