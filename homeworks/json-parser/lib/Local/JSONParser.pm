0b3a63cc00adab600ffa74e059e7619b824bae42
Dima <shedi26@mail.ru>
2016-05-02 23:26:41 +0300

    json-parser begin

--- homeworks/json-parser/lib/Local/JSONParser.pm
+++ homeworks/json-parser/lib/Local/JSONParser.pm
@@ -9,6 +9,13 @@ our @EXPORT = qw( parse_json );
 sub parse_json {
 	my $source = shift;
 	
+	my $value;
+	my $string = qr l"(?:\\["/bfnrt]|\\\\|u\d{4}|[^"\\])*"l;
+	my $number = qr l-?\d+(?:\.\d+)*(?:[eE][-+]?\d+)*l;
+	my $object = qr /\{(?:\s*$string\s*:\s*$value\s*)?(?:,\s*$string\s*:\s*$value\s*)*\}/;
+	my $array = qr /\[(?:\s*$value\s*)?(?:,\s*$value\s*)*\]/;
+	$value = qr /$string|$number|$object|$array|true|false|null/;
+
 	use JSON::XS;
 	
 	# return JSON::XS->new->utf8->decode($source);
