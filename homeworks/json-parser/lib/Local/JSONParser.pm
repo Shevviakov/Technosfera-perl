b1f252a6662a11224bc0e875253cba13492994fd
Dima <shedi26@mail.ru>
2016-05-07 22:09:11 +0300

    Task json_parser completed

--- homeworks/json-parser/lib/Local/JSONParser.pm
+++ homeworks/json-parser/lib/Local/JSONParser.pm
@@ -8,18 +8,59 @@ our @EXPORT = qw( parse_json );
 
 sub parse_json {
 	my $source = shift;
-	
-	my $value;
-	my $string = qr l"(?:\\["/bfnrt]|\\\\|u\d{4}|[^"\\])*"l;
-	my $number = qr l-?\d+(?:\.\d+)*(?:[eE][-+]?\d+)*l;
-	my $object = qr /\{(?:\s*$string\s*:\s*$value\s*)?(?:,\s*$string\s*:\s*$value\s*)*\}/;
-	my $array = qr /\[(?:\s*$value\s*)?(?:,\s*$value\s*)*\]/;
-	$value = qr /$string|$number|$object|$array|true|false|null/;
+
+	my $ans;
+	my $JSON = qr /
+		(?: (?&OBJECT) | (?&ARRAY) )(?{ $ans = $^R->[1] })
+		(?(DEFINE)
+			(?<VALUE>
+				\s*( (?&OBJECT) | (?&ARRAY) | (?&NUMBER) | (?&STRING) 
+					| true (?{ [$^R, 1] })
+					| false (?{ [$^R, ""] })
+					| null (?{ [$^R, undef] })
+				)\s*
+				
+			)
+			(?<OBJECT>
+				\{\s* (?{ [$^R, {}] })
+				(?: (?&KEYVALUE) #[[$^R, {}], "string", value]
+					(?{ [$^R->[0][0], {$^R->[1] => $^R->[2]}] })
+				 )?
+				(?> \s*,\s* (?&KEYVALUE)
+					(?{ $^R->[0][1]{$^R->[1]} = $^R->[2]; $^R->[0] })
+				)*
+				\s*\}
+			)
+			(?<KEYVALUE>
+				(?&STRING) #[$^R, "string"]
+				\s* : \s* (?&VALUE) #[[$^R, "string"], value]
+				(?{ [$^R->[0][0], $^R->[0][1], $^R->[1]] })
+			)
+			(?<ARRAY>
+				\[\s* (?{ [$^R, []] })
+				((?&VALUE) #[[$^R, []], value]
+				(?{ [$^R->[0][0], [$^R->[1]]] }))? \s*
+				
+				(?: ,\s* (?&VALUE) (?{ push @{$^R->[0][1]}, $^R->[1]; $^R->[0] }) )*
+				\s* \]
+			)
+			(?<STRING>
+				(" (?: \\["\/bnfrt] | \\u\d{4} | [^"\\] )* ")
+				(?{my $str = $^N; $str =~ s{\\u(\d{4})}(\\x\{$1\})g; [$^R, eval $str ] })
+			)
+			(?<NUMBER>
+				( -?\d+(?:\.\d+)? (?:[eE][-+]?\d+)? )
+				(?{ [$^R, 0+$^N] })
+			)
+		)
+	/x;
 
 	use JSON::XS;
 	
+	unless ($source =~ $JSON) {die "incorrect JSON";}
+	
 	# return JSON::XS->new->utf8->decode($source);
-	return {};
+	return $ans;
 }
 
 1;
